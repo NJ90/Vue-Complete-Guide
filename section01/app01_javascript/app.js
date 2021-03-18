8785af02a41702e275f59cf1688886024bd3defc
nameejang <jangnamee@gmail.com>
2021-03-18 17:13:35 +0900

    섹션 1: Getting Started

---
+++ section01/app01_javascript/app.js
@@ -0,0 +1,18 @@
+var buttonEl = document.querySelector("button")
+var inputEl = document.querySelector("#goal")
+var outputEl = document.querySelector("ul")
+
+function addGoal(){
+    var getInputValue = inputEl.value;
+    var createList = document.createElement("li");
+
+    console.log(getInputValue);
+
+    createList.textContent = getInputValue;
+    
+    outputEl.append(createList);
+    inputEl.value='';
+    
+}
+
+buttonEl.addEventListener("click", addGoal)
---
+++ section01/app01_vue/app.js
@@ -0,0 +1,34 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+            this.enteredValue='';
+        }
+    }
+}).mount('#app');
+
+
+// var buttonEl = document.querySelector("button")
+// var inputEl = document.querySelector("#goal")
+// var outputEl = document.querySelector("ul")
+
+// function addGoal(){
+//     var getInputValue = inputEl.value;
+//     var createList = document.createElement("li");
+
+//     console.log(getInputValue);
+
+//     createList.textContent = getInputValue;
+    
+//     outputEl.append(createList);
+//     inputEl.value='';
+    
+// }
+
+// buttonEl.addEventListener("click", addGoal)
