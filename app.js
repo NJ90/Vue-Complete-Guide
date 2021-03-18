29e10c618c692dc8e600a4d9a9dab0ed837864a6
nameejang <jangnamee@gmail.com>
2021-03-18 11:30:02 +0900

    6. Re-building the App with Vue

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var buttonEl = document.querySelector("button")
-var inputEl = document.querySelector("#goal")
-var outputEl = document.querySelector("ul")
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
 
-function addGoal(){
-    var getInputValue = inputEl.value;
-    var createList = document.createElement("li");
 
-    console.log(getInputValue);
+// var buttonEl = document.querySelector("button")
+// var inputEl = document.querySelector("#goal")
+// var outputEl = document.querySelector("ul")
 
-    createList.textContent = getInputValue;
+// function addGoal(){
+//     var getInputValue = inputEl.value;
+//     var createList = document.createElement("li");
+
+//     console.log(getInputValue);
+
+//     createList.textContent = getInputValue;
     
-    outputEl.append(createList);
-    inputEl.value='';
+//     outputEl.append(createList);
+//     inputEl.value='';
     
-}
+// }
 
-buttonEl.addEventListener("click", addGoal)
+// buttonEl.addEventListener("click", addGoal)
