4c624e50717b36562192b28e2808824e273cc50e
nameejang <jangnamee@gmail.com>
2021-03-19 12:43:21 +0900

    16. Understanding "methods" in Vue Apps

--- section02/app.js
+++ section02/app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
             courseGoal: 'Vue!',
             aboutLink: 'https://kr.vuejs.org/v2/guide/index.html'
         };
-    }
+    },
     // 문법2
     // data: function(){
     //     return{
     //         courseGoal: 'zzz'
     //     }
     // }
+
+    methods: {
+        outputGoal(){
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return 'Learn!'
+            } else {
+                return 'Nice!'
+            }
+
+        }
+    }
 });
 app.mount('#user-goal');
