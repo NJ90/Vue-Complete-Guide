f4f4e0bdbcac850dc942ae1526afb7e51d7b35cb
nameejang <jangnamee@gmail.com>
2021-03-19 15:30:10 +0900

    17. Working with Data inside of a Vue App

--- section02/app.js
+++ section02/app.js
@@ -1,26 +1,33 @@
 const app = Vue.createApp({
-    data() {
+    data() { //데이터 자체가 함수
         return {
             courseGoal: 'Vue!',
-            aboutLink: 'https://kr.vuejs.org/v2/guide/index.html'
+            aboutLink: 'https://kr.vuejs.org/v2/guide/index.html',
+            데이터불러오기1 : '데이터불러오기1',
+            데이터불러오기2 : '데이터불러오기2'
         };
     },
-    // 문법2
+    // data 문법2
     // data: function(){
     //     return{
     //         courseGoal: 'zzz'
     //     }
     // }
 
-    methods: {
+    methods: { //기능(함수) 모음, 때문에 html문서에서 함수로 호출한다
         outputGoal(){
             const randomNumber = Math.random();
-            if (randomNumber < 0.5){
-                return 'Learn!'
-            } else {
-                return 'Nice!'
-            }
+            // if (randomNumber < 0.5){
+            //     return 'Learn!'
+            // } else {
+            //     return 'Nice!'
+            // }
 
+            if (randomNumber < 0.5){//데이터에 있는 프로퍼티 불러오기
+                return this.데이터불러오기1
+            } else {
+                return this.데이터불러오기2
+            }
         }
     }
 });
