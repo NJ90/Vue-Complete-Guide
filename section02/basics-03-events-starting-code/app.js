758896bc56e10eed4409a71e5480bea583327877
nameejang <jangnamee@gmail.com>
2021-03-22 15:52:45 +0900

    23. Using the Native Event Object

--- section02/basics-03-events-starting-code/app.js
+++ section02/basics-03-events-starting-code/app.js
@@ -2,15 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      myName:''
     };
   },
 
-  methods: { //메소드에 arguments를 넘겨봄. 매개변수(parameter) 주목
-    counterAdd(parameter){
+  methods: { 
+    counterAdd(parameter){//메소드에 arguments를 넘겨봄. 매개변수(parameter) 주목
       this.counter = this.counter + parameter
     },
-    counterRemove(parameter){
+    counterRemove(parameter){//메소드에 arguments를 넘겨봄. 매개변수(parameter) 주목
       this.counter = this.counter - parameter
+    },
+    setName(event){
+      // this.myName = document.getElementsByTagName(input).value;
+      this.myName = event.target.value;
     }
   }
 });
