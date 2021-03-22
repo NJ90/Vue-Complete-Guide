3c1bb50ac9cd7ff929df46974629b17b01b1297e
nameejang <jangnamee@gmail.com>
2021-03-22 11:21:39 +0900

    21. Events & Methods

--- section02/basics-03-events-starting-code/app.js
+++ section02/basics-03-events-starting-code/app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+
+  methods: {
+    counterAdd(){
+      this.counter = this.counter + 1
+    },
+    counterRemove(){
+      this.counter = this.counter - 1
+    }
+  }
 });
 
 app.mount('#events');
