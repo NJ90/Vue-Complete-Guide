29133835cd2cff14808d93fc07044c1e66df731b
nameejang <jangnamee@gmail.com>
2021-03-22 16:57:25 +0900

    24. Exploring Event Modifiers

--- section02/basics-03-events-starting-code/app.js
+++ section02/basics-03-events-starting-code/app.js
@@ -2,11 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      myName:''
+      myName:'',
+      confirmedName:''
     };
   },
 
   methods: { 
+    confirmName(){
+      this.confirmedName = this.myName;
+      console.log('Confirmed');
+    },
+    submitForm(event){
+      // event.preventDefault();
+      alert('Submitted');
+    },
     counterAdd(parameter){//메소드에 arguments를 넘겨봄. 매개변수(parameter) 주목
       this.counter = this.counter + parameter
     },
