e2d93947b955d6b56954ea48261e6e2de7297b0c
nameejang <jangnamee@gmail.com>
2021-03-24 16:54:35 +0900

    29. Working with Watchers

--- section02/basics-05-using-the-native-event-object/app.js
+++ section02/basics-05-using-the-native-event-object/app.js
@@ -2,17 +2,36 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName:'',
+      fullname:''
     };
   },
-  computed:{//computed is similar with method, but it can use like a data property, not like a method.
-    fullname(){//computed property덕분에 name 부분 함수가 독립적으로 동작함을 알 수 있음
-      console.log('Running again..');
-      if (this.name === ''){
-        return '';
+  watch:{
+    //method를 정의하여 사용
+    name(value){
+      if(value === ''){
+        this.fullname = '';
+      } else {
+        this.fullname = value + '' + 'JANG'
       }
-      return this.name + '' + 'JANG'
+    },
+    lastName(value){
+      if(value === ''){
+        this.fullname = '';
+      } else {
+        this.fullname = this.name + '' + value;
+      }
     }
+  },
+  computed:{//computed is similar with method, but it can use like a data property, not like a method.
+    // fullname(){//computed property덕분에 name 부분 함수가 독립적으로 동작함을 알 수 있음
+    //   console.log('Running again..');
+    //   if (this.name === ''){
+    //     return '';
+    //   }
+    //   return this.name + '' + 'JANG'
+    // }
   },
   methods: {
     outputFullname(){
