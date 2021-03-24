abd089ffa08bd2792c22e2d747f53c9d8e5d6b9d
nameejang <jangnamee@gmail.com>
2021-03-24 16:36:02 +0900

    28. Introducing Computed Properties

--- section02/basics-05-using-the-native-event-object/app.js
+++ section02/basics-05-using-the-native-event-object/app.js
@@ -5,7 +5,23 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed:{//computed is similar with method, but it can use like a data property, not like a method.
+    fullname(){//computed property덕분에 name 부분 함수가 독립적으로 동작함을 알 수 있음
+      console.log('Running again..');
+      if (this.name === ''){
+        return '';
+      }
+      return this.name + '' + 'JANG'
+    }
+  },
   methods: {
+    outputFullname(){
+      console.log('Running again..');
+      if (this.name === ''){
+        return '';
+      }
+      return this.name + '' + 'JANG'
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
     },
